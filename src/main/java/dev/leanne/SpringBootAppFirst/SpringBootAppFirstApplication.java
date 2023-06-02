5ed2b95b371d56a5aeb96f9a262fe6159a3a3794
Leanne Gass <leannegass@gmail.com>
2023-06-02 11:39:57 +0100

    innitial commit

---
+++ src/main/java/dev/leanne/SpringBootAppFirst/SpringBootAppFirstApplication.java
@@ -0,0 +1,13 @@
+package dev.leanne.SpringBootAppFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/leanne/SpringBootAppFirst/SpringBootAppFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leanne.SpringBootAppFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
